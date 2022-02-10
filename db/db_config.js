36bd72710cdd15caa4dc1738e2dd4e85544de5b3
Charles Wu <wucharleswu@gmail.com>
2022-02-10 18:11:37 -0500

    Update db_config.js

--- db/db_config.js
+++ db/db_config.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const db = {};
 db.mongoose = mongoose;
-db.url = process.env.MONGODB_URI;
+// db.url = process.env.MONGODB_URI;
 
-// db.url = 'mongodb://localhost:27017/test';
+db.url = 'mongodb://127.0.0.1:27017/test';
 
 module.exports = db;
